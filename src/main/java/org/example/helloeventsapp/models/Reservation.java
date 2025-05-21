a186621b92ada1f488efbaa40cc2ee66485fec56
ridaDev <kiratterida@gmail.com>
2025-05-21 10:33:29 +0100

    créer Entity Reservation

--- src/main/java/org/example/helloeventsapp/models/Reservation.java
+++ src/main/java/org/example/helloeventsapp/models/Reservation.java
@@ -1,4 +1,22 @@
 package org.example.helloeventsapp.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import org.springframework.data.annotation.Id;
+
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String dateReservation;
+    private int nombrePlaces;
+
+    @ManyToOne
+    private Client client;
+
+    @ManyToOne
+    private Evenement evenement;
 }
