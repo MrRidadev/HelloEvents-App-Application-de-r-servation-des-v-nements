ec1e2c7eefa70ee6a07596e5ee8f14e608bb5254
ridaDev <kiratterida@gmail.com>
2025-05-21 10:30:40 +0100

    créer Entity Client

--- src/main/java/org/example/helloeventsapp/models/Client.java
+++ src/main/java/org/example/helloeventsapp/models/Client.java
@@ -1,4 +1,14 @@
 package org.example.helloeventsapp.models;
 
-public class Client {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+public class Client extends Utilisateur {
+
+    @OneToMany(mappedBy = "client")
+    private List<Reservation> reservations;
 }
