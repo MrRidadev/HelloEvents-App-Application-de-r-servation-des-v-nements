2a5d3d6895429d8c744cbc51e97b19b14f6d745d
ridaDev <kiratterida@gmail.com>
2025-05-23 10:08:43 +0100

    créer controller findAll en evenement

--- src/main/java/org/example/helloeventsapp/controllers/EvenementController.java
+++ src/main/java/org/example/helloeventsapp/controllers/EvenementController.java
@@ -1,4 +1,25 @@
 package org.example.helloeventsapp.controllers;
 
+import org.example.helloeventsapp.models.Evenement;
+import org.example.helloeventsapp.services.EvenementService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/evenement")
 public class EvenementController {
+
+    private EvenementService evenementService;
+
+    public EvenementController(EvenementService evenementService) {
+        this.evenementService = evenementService;
+    }
+
+    @GetMapping("/get")
+    public List<Evenement> getEvenements() {
+        return evenementService.findAll();
+    }
 }
