61607a31e4772919d60634dac7815ad5cfd1cd68
ridaDev <kiratterida@gmail.com>
2025-05-28 12:23:09 +0100

    créer structure de front end

--- src/main/java/org/example/helloeventsapp/repository/ClientRepository.java
+++ src/main/java/org/example/helloeventsapp/repository/ClientRepository.java
@@ -2,10 +2,12 @@ package org.example.helloeventsapp.repository;
 
 import org.example.helloeventsapp.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
     Client findByEmail(String email);
+
 }
